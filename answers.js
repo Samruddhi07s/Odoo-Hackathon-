ff860258ec6279d541321b9a90aedb352b2f8730
Samruddhi07s <samruddhisavane@gmail.com>
2025-07-12 17:34:06 +0530

    Delete answers.js

--- answers.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { postAnswer, getAnswersByQuestionId } = require('../controllers/answersController');
-
-router.get('/:questionId', getAnswersByQuestionId);
-router.post('/', postAnswer);
-
-module.exports = router;
