b55c187a76f8a7f7597f5d213b3c57c5048b2960
Samruddhi07s <samruddhisavane@gmail.com>
2025-07-12 17:33:52 +0530

    Delete questions.js

--- questions.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAllQuestions, postQuestion } = require('../controllers/questionsController');
-
-router.get('/', getAllQuestions);
-router.post('/', postQuestion);
-
-module.exports = router;
